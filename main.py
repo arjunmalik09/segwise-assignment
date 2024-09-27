b83dfa36ee1f6d881bf1e20b46644edd966c06a2
Arjun Malik <arjunmalik@Arjuns-MacBook-Air.local>
2024-09-27 17:07:37 +0530

    update main

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 # Given the data of some of the android apps from playstore, this pyspark job generates insights from it.
 # Insights provide count of apps based on various combination of fields provided in input.
+# 1. Code complete
+# 2. Testing
+# 3. Write output as file and submit
 import sys
 from operator import add
 
@@ -12,7 +15,7 @@ if __name__ == "__main__":
 
     spark = SparkSession\
         .builder\
-        .appName("PythonCounts")\
+        .appName("Counts")\
         .getOrCreate()
 
     lines = spark.read.text(sys.argv[1]).rdd.map(lambda r: r[0])
@@ -20,7 +23,9 @@ if __name__ == "__main__":
                   .map(lambda x: (x, 1)) \
                   .reduceByKey(add)
     output = counts.collect()
-    for (word, count) in output:
-        print("%s: %i" % (word, count))
+    with open("output.csv", "rw+") as output_file:
+        for (word, count) in output:
+            print("%s: %i" % (word, count))
+            output_file.write(word, count)
 
     spark.stop()
