6de1f095497d5af772597a4f4b6fbd093b4dc773
Arjun Malik <arjunmalik.cool96@gmail.com>
2024-09-28 14:13:33 +0000

    update main with transform function

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
 # Given the data of some of the android apps from playstore, this pyspark job generates insights from it.
 # Insights provide count of apps based on various combination of fields provided in input.
 import sys
-from operator import add
-
 from pyspark.sql import SparkSession
+import pandas as pd
+
+def transform(df: pd.DataFrame) -> pd.DataFrame:
+    pass
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: Insigts <file>", file=sys.stderr)
+        print("Usage: python main.py <csv-file-name>", file=sys.stderr)
+        sys.exit(-1)
+    filename = sys.argv[1]
+    if filename[-4:] != ".csv":
+        print("Invalid Input File: Please input csv file", file=sys.stderr)
         sys.exit(-1)
 
-    spark = SparkSession\
-        .builder\
-        .appName("Counts")\
-        .getOrCreate()
-
-    lines = spark.read.text(sys.argv[1]).rdd.map(lambda r: r[0])
-    counts = lines.flatMap(lambda x: x.split(',')) \
-                  .map(lambda x: (x, 1)) \
-                  .reduceByKey(add)
-    output = counts.collect()
-    with open("output.csv", "rw+") as output_file:
-        for (word, count) in output:
-            print("%s: %i" % (word, count))
-            output_file.write(word, count)
-
+    spark = SparkSession.builder.appName("Counts").getOrCreate()
+    df = spark.read.csv(filename)
+    df.applyInPandas(transform, schema=df.schema)
+    df.show()
+    # counts = lines.flatMap(lambda x: x.split(',')).map(lambda x: (x, 1))
+    # output = counts.collect()
+    # with open("output.csv", "rw+") as output_file:
+    #     for (word, count) in output:
+    #         print("%s: %i" % (word, count))
+    #         output_file.write(word, count)
     spark.stop()
