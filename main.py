3782bd55dd83949c9f3537b41ed3e80302e6dd88
Arjun Malik <arjunmalik@Arjuns-MacBook-Air.local>
2024-10-01 11:20:30 +0530

    tranform dataframe in main

--- main.py
+++ main.py
@@ -1,18 +1,34 @@
 # Given the data of some of the android apps from playstore, this pyspark job generates insights from it.
 # Insights provide count of apps based on various combination of fields provided in input.
 import sys
+from itertools import combinations
+import time
 from pyspark.sql import SparkSession
+
+def bin_dataframe(spark, df):
+    # from pyspark.ml.feature import QuantileDiscretizer
+    # output_column_names = [col + "_bin" for col in column_names]
+    # qds = QuantileDiscretizer(relativeError=0.01, handleInvalid="error", numBuckets=10, inputCols=column_names, outputCols=output_column_names)
+    # binner = lambda df: qds.setHandleInvalid("keep").fit(df).transform(df)
+    return df
 
 def transform(spark, df):
     """
     1. Bin each integer column of dataframe. Create 10 bin for each integer column
     2. For each k combination out of n columns,
         1. Group the k column combination and find total count
+    3. Return the dataframes
     """
+    tranformed_dfs = []
     column_names = df.columns
-    rows = df.offset(0).limit(100).collect()
-    data = [{col: row[col]} for col in column_names for row in rows]
-    return spark.createDataFrame(data)
+    df = bin_dataframe(spark, df)
+    for k in range(1, len(column_names) + 1):
+        print(f"Tranform dataframe as {k} length")
+        for column_combination in combinations(column_names, k):
+            tranformed_dfs.append(
+                df.select(column_combination).groupBy(column_combination).count()
+            )
+    return tranformed_dfs
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -22,9 +38,10 @@ if __name__ == "__main__":
     if filename[-4:] != ".csv":
         print("Invalid Input File: Please input csv file", file=sys.stderr)
         sys.exit(-1)
-    output_filename = f"output_{filename}"
     spark = SparkSession.builder.appName("Counts").getOrCreate()
     df = spark.read.csv(filename)
-    output_df = transform(spark, df)
-    output_df.write.csv(output_filename, header=True)
+    output_dfs = transform(spark, df)
+    for output_df in output_dfs:
+        output_filename = f"output_{int(time.time() * 1000)}_{filename}"
+        output_df.write.csv(output_filename, header=True)
     spark.stop()
